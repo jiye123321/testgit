49209d35cc77d958646aff30e9274323f13a462c
jiye123321 <1530161261@qq.com>
2017-04-12 20:15:19 +0800

    lianxi

---
+++ lianxi/03-23/move2.js
@@ -0,0 +1,50 @@
+
+function getStyle(obj,name)     //去除offsetLeft
+{
+	if(obj.currentStyle)
+	{
+		return obj.currentStyle[name];
+	}
+	else
+	{
+		return getComputedStyle(obj,false)[name];
+	}
+};
+function startMove(obj,attr,iTarget,fnEnd)
+{
+	clearInterval(obj.timer);
+	obj.timer=setInterval(function(){
+		var cur=0;     //parseInt:把字符串转换为整数
+		
+		if(attr=='opacity')
+		{
+			cur=Math.round(parseFloat(getStyle(obj,attr))*100);//因为计算机算的不精确，可能会出现小数，所以四舍五入精确下
+		}
+		else
+		{
+			cur=parseInt(getStyle(obj,attr));
+		}
+		var speed=(iTarget-cur)/6;
+		speed=speed>0?Math.ceil(speed):Math.floor(speed);
+		
+		if(cur==iTarget)
+		{
+			clearInterval(obj.timer);
+			
+			if(fnEnd)
+			fnEnd();
+		}
+		else
+		{
+			if(attr=='opacity')
+			{
+				obj.style.filter='alpha(opcity:'+(cur+speed)+')';
+				obj.style.opacity=(cur+speed)/100;
+			}
+			else
+			{
+				obj.style[attr]=cur+speed+'px';
+			}
+		}
+		},30);
+};
